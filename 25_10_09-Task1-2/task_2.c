fa522bb39af8ebabb27a188c1336c5558576fd9b
Emelin <nrnemelinm@gmail.com>
2025-10-12 16:55:59 +0800

    initial commit

--- 25_10_09-Task1-2/task_2.c
+++ 25_10_09-Task1-2/task_2.c
@@ -3,31 +3,27 @@ using namespace std;
 
 struct Node {
     string data;
-    Node* next;    // Pointer to next node
-    Node* prev;    // Pointer to previous node
+    Node* next;
+    Node* prev;
 };
 
 int main() {
-    // Create nodes
+
     Node node1, node2, node3;
     
-    // Initialize node1 (head)
     node1.data = "test1";
-    node1.prev = NULL;      // First node, no previous
-    node1.next = &node2;    // Points to node2
+    node1.prev = NULL;
+    node1.next = &node2;
     
-    // Initialize node2
     node2.data = "test2";
-    node2.prev = &node1;    // Points back to node1
-    node2.next = &node3;    // Points to node3
+    node2.prev = &node1;
+    node2.next = &node3;
     
-    // Initialize node3 (tail)
     node3.data = "test3";
-    node3.prev = &node2;    // Points back to node2
-    node3.next = NULL;      // Last node, no next
+    node3.prev = &node2;
+    node3.next = NULL;
     
-    // Forward traversal
-    cout << "Forward traversal: ";
+    cout << "Forward: ";
     Node* current = &node1;
     while (current != NULL) {
         cout << current->data << " ";
@@ -35,8 +31,7 @@ int main() {
     }
     cout << endl;
     
-    // Backward traversal
-    cout << "Backward traversal: ";
+    cout << "Backwards: ";
     current = &node3;
     while (current != NULL) {
         cout << current->data << " ";
